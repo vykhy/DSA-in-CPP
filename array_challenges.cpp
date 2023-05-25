4a870d80a5a493cf104e68bbf539bfd804203195
vykhy <vikehietoyhoshii@gmail.com>
2023-05-25 11:46:53 +0530

    8.4 array challenges

--- array_challenges.cpp
+++ array_challenges.cpp
@@ -1,6 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+// video 8.4
+
 void printArr(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -26,20 +28,76 @@ int sumOfSubArray(int arr[], int n)
     return sum;
 }
 
+int lengthOfLongestArithmeticSubarray(int arr[], int n)
+{
+    int longest = 0;
+    int prev_difference = arr[1] - arr[0];
+    int j = 2;
+    int curr = 2;
+
+    while (j < n)
+    {
+        if (arr[j] - arr[j - 1] == prev_difference)
+        {
+            curr++;
+        }
+        else
+        {
+            prev_difference = arr[j] - arr[j - 1];
+            curr = 2;
+        }
+        longest = max(longest, curr);
+        j++;
+    }
+
+    return longest;
+}
+
+int recordBreakingDays(int arr[], int n)
+/**
+ * returns number of days where visitors were more than any previous day and more than its following day.
+ * First and last day could be record breaking days
+ */
+{
+    int days = 0;
+    int record = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // first day more than 2nd day
+        if (i == 0 && arr[1] < arr[0])
+        {
+            record = arr[0];
+            days++;
+        }
+        // last day is highest
+        else if (i == n - 1 && arr[i] > record)
+        {
+            days++;
+            record = arr[i];
+        }
+        // if highest to date and more than next day
+        else if (arr[i] > record && arr[i + 1] < arr[i])
+        {
+            record = arr[i];
+            days++;
+        }
+    }
+    return days;
+}
+
 int main()
 {
-    int arr2[5] = {
-        1, 1, 1, 1, 1};
     int arr[20] = {
         76,
         13,
         325,
-        456,
         0,
         45,
         23,
+        456,
         32,
         34,
+        765,
         35,
         1,
         876,
@@ -47,12 +105,13 @@ int main()
         3,
         4,
         5,
-        765,
         56,
         58,
         72};
 
-    int result = sumOfSubArray(arr2, 5);
+    // int result = sumOfSubArray(arr, 20);
+    // int result = lengthOfLongestArithmeticSubarray(arr, 20);
+    int result = recordBreakingDays(arr, 20);
     cout << result << endl;
     // printArr(arr, 20);
 
